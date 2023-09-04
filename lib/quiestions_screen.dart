3501f9610671b6483fd3c0e0c74ac70e89065b53
Orkhan Guseinov <orxan4ik7777@gmail.com>
2023-09-04 20:31:58 +0400

    Container + margin EdgeInsets.all(20)

--- lib/quiestions_screen.dart
+++ lib/quiestions_screen.dart
@@ -15,18 +15,21 @@ class _QuiestionsScreenState extends State<QuiestionsScreen> {
     final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(color: Colors.white),
-          ),
-          const SizedBox(height: 30),
-          ...currentQuestion.answers
-              .map((answer) => AnswerButton(answerText: answer, onTap: () {}))
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.answers
+                .map((answer) => AnswerButton(answerText: answer, onTap: () {}))
+          ],
+        ),
       ),
     );
   }
